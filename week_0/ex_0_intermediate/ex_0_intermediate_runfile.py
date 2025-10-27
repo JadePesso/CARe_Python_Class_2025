6262caa664a77466bbc632e042a28b53f59900b5
JadePesso <jadepesso@icloud.com>
2025-10-27 11:15:34 +0100

    hi, my homework for intermediate_ex0 is here!

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,13 @@
-#YOUR CODE FOR EX_0 INTERMEDIATE HERE
+import math
+initial_count=float(input("Enter initial cell count"))
+Enter initial cell count>? 10000
+final_count=float(input("enter final cell count"))
+enter final cell count>? 90000
+time=float(input("Enter time elapsed"))
+Enter time elapsed>? 10
+import math
+growth_rate=(math.log(final_count)-math.log(initial_count))/time
+print(growth_rate)
+0.2197224577336218
+
+
